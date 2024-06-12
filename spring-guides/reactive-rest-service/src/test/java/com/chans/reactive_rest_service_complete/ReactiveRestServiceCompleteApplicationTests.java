729cafcf427edf5b35ad2cdf66ce06f9547d8ea5
c2chance <1990542+c2chance@users.noreply.github.com>
2024-06-12 17:30:25 +0800

    add reactiveRestService project

---
+++ spring-guides/reactive-rest-service/src/test/java/com/chans/reactive_rest_service_complete/ReactiveRestServiceCompleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chans.reactive_rest_service_complete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveRestServiceCompleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
