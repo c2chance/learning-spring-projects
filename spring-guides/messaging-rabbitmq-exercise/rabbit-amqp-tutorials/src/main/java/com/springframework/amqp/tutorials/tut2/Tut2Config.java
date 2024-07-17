f4352235d7d2c9acb87dd9b933163ef11dbc7b51
c2chance <1990542+c2chance@users.noreply.github.com>
2024-07-17 17:54:10 +0800

    tut3 finished

--- spring-guides/messaging-rabbitmq-exercise/rabbit-amqp-tutorials/src/main/java/com/springframework/amqp/tutorials/tut2/Tut2Config.java
+++ spring-guides/messaging-rabbitmq-exercise/rabbit-amqp-tutorials/src/main/java/com/springframework/amqp/tutorials/tut2/Tut2Config.java
@@ -1,5 +1,6 @@
 package com.springframework.amqp.tutorials.tut2;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -14,7 +15,7 @@ public class Tut2Config {
     }
 
     @Profile("receiver")
-    private static class Receiverconfig {
+    private static class ReceiverConfig {
 
         @Bean
         public Tut2Receiver receiver1() {
