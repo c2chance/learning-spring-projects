863c609a80242ddcfb7ea0e792ba5cc1c395f41c
c2chance <chance.chih@gmail.com>
2024-07-19 22:24:35 +0800

    add queue profile in Tut1Receiver

--- spring-guides/messaging-rabbitmq-exercise/rabbit-amqp-tutorials/src/main/java/com/springframework/amqp/tutorials/tut6/Tut6Config.java
+++ spring-guides/messaging-rabbitmq-exercise/rabbit-amqp-tutorials/src/main/java/com/springframework/amqp/tutorials/tut6/Tut6Config.java
@@ -1,5 +1,6 @@
 package com.springframework.amqp.tutorials.tut6;
 
+import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
